83438af59092e706140ef4eb90b46ed4e300ab45
rahul-1729 <rk9763981@gmail.com>
2024-06-18 07:50:56 +0530

    change target content

--- src/button.jsx
+++ src/button.jsx
@@ -2,20 +2,11 @@ function Button()
 {  
      let count =0;
 
-    const handleClick2 =(name)=> {
-      if(count<3)
-      {
-        count++;
-        console.log(`${name} you clicked me ${count} times`);
-      }
-      else
-      {
-        console.log(`${name} stop clicking me`);
-      }
-    }
+    const handleClick =(e)=>  e.target.textContent = "Ouch!";
+      
 
     return(
-        <button onClick ={()=>handleClick2("Tekken")}>
+        <button onClick ={(e)=>handleClick(e)}>
           Click Me  
         </button>
     );
